bd8e1648e86dc2df276a725c894c1b42af361de8
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-24 15:57:40 -0500

    creacion de la vista de edit

--- Controllers/LibroController.cs
+++ Controllers/LibroController.cs
@@ -11,31 +11,10 @@ namespace Biblioteca.Controllers
             return View();
         }
 
-        // GET: LibroController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: LibroController/Create
         public ActionResult Create()
         {
             return View();
-        }
-
-        // POST: LibroController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
 
         // GET: LibroController/Edit/5
@@ -44,40 +23,11 @@ namespace Biblioteca.Controllers
             return View();
         }
 
-        // POST: LibroController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         // GET: LibroController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: LibroController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
